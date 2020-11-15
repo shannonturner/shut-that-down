4dca54f0bcaa7663360ddb91977a253aed3cc0a7
Shannon Turner <shannon.v.turner@gmail.com>
2020-11-15 10:36:45 -0500

    Updated urls.py to not use old-style patterns

--- urls.py
+++ urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 from apps.shutthatdown.views import HomeView, QuoteView, StateView, BrowseView, SubmitView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomeView.as_view()),
     url(r'^quote/(?P<quote>[0-9]+)?$', QuoteView.as_view()),
@@ -13,4 +13,4 @@ urlpatterns = patterns('',
     url(r'^browse/$', QuoteView.as_view()), # When no quote type is specified, show a random quote
     url(r'^browse/(?P<type>[a-z\-]+)$', BrowseView.as_view()),
     url(r'^submit/$', SubmitView.as_view()),
-)
+]
